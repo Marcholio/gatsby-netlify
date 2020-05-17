b016e7aa02bc1989f7cfa02ba0683c01565d4637
Markus <markus.tyrkko@gmail.com>
2020-05-17 15:57:04 +0300

    Show blog on site

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
